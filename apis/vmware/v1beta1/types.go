5cd32793bc50aecd1d88167307a0c208e6a7cdff
Aarti <aartij17@gmail.com>
2021-11-03 11:21:58 -0700

    add condition constants

--- apis/vmware/v1beta1/types.go
+++ apis/vmware/v1beta1/types.go
@@ -16,6 +16,13 @@ limitations under the License.
 
 package v1beta1
 
+const (
+	// MachineFinalizer allows ReconcileWCPMachine to clean up
+	// resources associated with VSphereMachine before removing it from the
+	// API Server.
+	MachineFinalizer = "vmware.infrastructure.cluster.x-k8s.io"
+)
+
 // VSphereMachineTemplateResource describes the data needed to create a VSphereMachine from a template
 type VSphereMachineTemplateResource struct {
 	// Spec is the specification of the desired behavior of the machine.
@@ -24,3 +31,27 @@ type VSphereMachineTemplateResource struct {
 
 // VirtualMachineState describes the state of a VM.
 type VirtualMachineState string
+
+const (
+	// VirtualMachineStateNotFound is the string representing a VM that cannot be located.
+	VirtualMachineStateNotFound = VirtualMachineState("notfound")
+
+	// VirtualMachineStatePending is the string representing a VM with an in-flight task.
+	VirtualMachineStatePending = VirtualMachineState("pending")
+
+	// VirtualMachineStateCreated is the string representing a VM that's been created
+	VirtualMachineStateCreated = VirtualMachineState("created")
+
+	// VirtualMachineStatePoweredOn is the string representing a VM that has successfully powered on
+	VirtualMachineStatePoweredOn = VirtualMachineState("poweredon")
+
+	// VirtualMachineStateReady is the string representing a powered-on VM with reported IP addresses.
+	VirtualMachineStateReady = VirtualMachineState("ready")
+
+	// VirtualMachineStateDeleting is the string representing a machine that still exists, but has a deleteTimestamp
+	// Note that once a VirtualMachine is finally deleted, its state will be VirtualMachineStateNotFound
+	VirtualMachineStateDeleting = VirtualMachineState("deleting")
+
+	// VirtualMachineStateError is reported if an error occurs determining the status
+	VirtualMachineStateError = VirtualMachineState("error")
+)
